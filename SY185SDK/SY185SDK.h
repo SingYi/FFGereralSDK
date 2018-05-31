1c0618b5f97fa1c7be85a91d0f0bb2b359035f61
Sing.Yi <23323231@qq.com>
2018-05-31 17:23:38 +0800

    Initial Commit

---
+++ SY185SDK/SY185SDK.h
@@ -0,0 +1,19 @@
+//
+//  SY185SDK.h
+//  SY185SDK
+//
+//  Created by 燚 on 2018/5/31.
+//  Copyright © 2018年 Sans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SY185SDK.
+FOUNDATION_EXPORT double SY185SDKVersionNumber;
+
+//! Project version string for SY185SDK.
+FOUNDATION_EXPORT const unsigned char SY185SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SY185SDK/PublicHeader.h>
+
+
